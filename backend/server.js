f637402c25848b14f989eb38e4ebdb54527a1777
lucia <laysalvi@gmail.com>
2025-04-07 00:56:25 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -265,7 +265,7 @@ app.get('/api/vehiculos/patentes', async (req, res) => {
   try {
     const { data, error } = await supabase
       .from('vehiculos')
-      .select('id, patente');
+      .select('patente, marca');
     
     if (error) throw error;
     res.status(200).json(data);

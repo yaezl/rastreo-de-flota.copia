7f35870b0049a1e9160e22d353acc1b5db8d8d6b
lucia <laysalvi@gmail.com>
2025-05-04 18:21:47 -0300

    prueba abrir desde el celular

---
+++ frontend/service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+    console.log('SW instalado');
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', event => {
+    console.log('SW activado');
+  });
+  
+  self.addEventListener('fetch', event => {
+    // Permite que todo pase por el navegador normalmente
+  });
+  
